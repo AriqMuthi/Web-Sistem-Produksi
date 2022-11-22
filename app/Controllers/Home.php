8ec82bc345a894670831ad83faf67d121fa2aa5a
AriqMuthi <ariqmuthi22@gmail.com>
2022-11-22 19:33:09 +0700

    Menambah Form Input dan Form Repair, serta menambah tampilan Layout

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,25 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        $data=[
+            'title'=>'Data - Tampilan Data'
+        ];
+        return view('pages/data',$data);
+    }
+
+    public function input()
+    {
+        $data=[
+            'title'=>'Input - Form Input'
+        ];
+        return view('pages/input',$data);
+    }
+
+    public function repair()
+    {
+        $data=[
+            'title'=>'Repair - Form Repair'
+        ];
+        return view('pages/repair',$data);
     }
 }
---
+++ app/Views/pages/data.php
@@ -0,0 +1,11 @@
+<?= $this->extend('layout/layout'); ?>
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+        <h1>Halo ini repair</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error repellendus, quidem earum tenetur illum rerum nostrum! Facere voluptatem totam, eveniet exercitationem placeat alias odio eius consectetur doloremque. Tenetur, unde reprehenderit!</p>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
